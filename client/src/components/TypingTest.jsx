af864ca5bc28084fcccb60b63ece56d66e1d52cf
notPHPuser <artem2005dm@mail.ru>
2024-08-10 14:58:55 +0300

    надо доделаиь

--- client/src/components/TypingTest.jsx
+++ client/src/components/TypingTest.jsx
@@ -1,27 +1,25 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { setInput, incrementErrors, setWPM, setStartTime, reset } from '../store/typingSlice';
-import styled from 'styled-components';
+import s from './TypingTest.module.css';
 
-// Массив русских слов
 const russianWords = ['мама', 'папа', 'сын', 'дочь', 'бабушка', 'кот', 'собака'];
 
-// Выбираем 10 случайных слов из массива
 const randomWords = russianWords.sort(() => Math.random() - 0.5).slice(0, 10);
 
-// Объединяем слова в строку
 const TEXT = randomWords.join(' ');
 
 const TypingTest = () => {
   const dispatch = useDispatch();
   const { input, errors, wpm, startTime } = useSelector((state) => state.typing);
+  const [text, setText] = useState(TEXT);
 
   useEffect(() => {
     if (input.length === 0) {
       dispatch(reset());
     }
     if (startTime) {
-      const elapsedTime = (new Date() - startTime) / 1000; // Время в секундах
+      const elapsedTime = (new Date() - startTime) / 1000;
       const wordsTyped = input.split(' ').length;
       const wpmValue = Math.round((wordsTyped / elapsedTime) * 60);
       dispatch(setWPM(wpmValue));
@@ -36,42 +34,38 @@ const TypingTest = () => {
       dispatch(setStartTime(new Date()));
     }
 
-    // Проверка на ошибки
     if (value[value.length - 1] !== TEXT[input.length]) {
       dispatch(incrementErrors());
     }
   };
 
-  const Main = styled.div`
-    width: 100%;
-    // display: flex;
-    height: 100vh;
-    // flex-direction: colomn;
-    align-items: center;
-    text-align: center;
-  `;
+  const reload = () => {
+    setText(input);
+    dispatch(reset());
+  };
 
   return (
-    <div>
-      <h1>Тест на скорость печати</h1>
+    <div className={s.all}>
+      <h1 className={s.test}>Тест на скорость печати</h1>
       {/* <p style={{ color: 'grey' }}>{TEXT}</p> */}
 
-      <div>
-        <h2>Подсветка:</h2>
+      <div className={s.type}>
+        <h2>Текст:</h2>
         {TEXT.split('').map((char, index) => (
           <span
+            className={s.text}
             key={index}
             style={{
-              color: index < input.length ? (input[index] === char ? 'green' : 'red') : 'black',
+              color: index < input.length ? (input[index] === char ? 'green' : 'red') : 'white  ',
             }}
           >
             {char}
           </span>
         ))}
       </div>
-      <input type='text' value={input} onChange={handleInputChange} />
+      <input className={s.input} type='text' value={input} onChange={handleInputChange} />
       {input.length === TEXT.length && (
-        <div>
+        <div className={s.result}>
           <h2>Результаты:</h2>
           <p>Ошибки: {errors}</p>
           <p>Скорость печати (WPM): {wpm}</p>
